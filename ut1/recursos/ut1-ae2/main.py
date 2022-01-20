57e325e96cefc7cad3393b352c7eeb464099af88
Iván Chillón García <90761910+ichigar@users.noreply.github.com>
2022-01-20 21:01:57 +0000

    CM

--- ut1/recursos/ut1-ae2/main.py
+++ ut1/recursos/ut1-ae2/main.py
@@ -1,24 +1,24 @@
 from a1 import *
-# print(valid_passwd("lej50*paRapa"))
-# print(valid_passwd("lej50*pa"))
-# print(valid_passwd('12345'))
+print(valid_passwd("lej50*paRapa"))
+print(valid_passwd("lej50*pa"))
+print(valid_passwd('12345'))
 
 from a2 import *
 
-# count_phrases("Una frase. Dos frases. Tres frases. ")
-# phrases_in_text("Una frase. Dos frases. Tres, cuatro, cinco; seis. Y siete.")
-# ordered_words("Uno dos tres cuatro Cinco")
-# ordered_words("Uno, dos, tres; cuatro. Cinco")
-# n_words_end_in("o", "Uno dos tres cuatro Cinco")
-# n_words_end_in("o", "Uno, dos, tres; cuatro. Cinco")
+count_phrases("Una frase. Dos frases. Tres frases. ")
+phrases_in_text("Una frase. Dos frases. Tres, cuatro, cinco; seis. Y siete.")
+ordered_words("Uno dos tres cuatro Cinco")
+ordered_words("Uno, dos, tres; cuatro. Cinco")
+n_words_end_in("o", "Uno dos tres cuatro Cinco")
+n_words_end_in("o", "Uno, dos, tres; cuatro. Cinco")
 
 from a3 import *
-# monedas = 4
-# n_tiradas = 5
-# p_final = partida(monedas, n_tiradas)
-# print(f"Has finalizado con {p_final} monedas")
-# delete_word("dos", "Uno dos tres cuatro Cinco")
-# delete_word("dos", "Uno, dos, tres; cuatro. Cinco")
+monedas = 4
+n_tiradas = 5
+p_final = partida(monedas, n_tiradas)
+print(f"Has finalizado con {p_final} monedas")
+delete_word("dos", "Uno dos tres cuatro Cinco")
+delete_word("dos", "Uno, dos, tres; cuatro. Cinco")
 
 from a4 import *
 catalogo = [["camiseta roja", 10.50], ["camiseta azul", 10.99], ["camiseta blanca", 9.90],["pantalon vaquero", 21.50], ["pantalon corto", 13.50], ["chaleco", 17.20]]
@@ -27,9 +27,9 @@ carrito = [["camiseta roja", 1],["pantalon corto", 2], ["chaleco", 3]]
 p_chaleco = obtener_precio(catalogo, "chaleco")
 print(f"El precio del chaleco es de {p_chaleco}€.\n")
 
-# total = total_compra(catalogo, carrito)
-# print(f"El total de la compra es de: {total}€.\n")
+total = total_compra(catalogo, carrito)
+print(f"El total de la compra es de: {total}€.\n")
             
-# print("El extracto de la compra en formato HTML es:\n")
+print("El extracto de la compra en formato HTML es:\n")
 
-# mostrar_compra(catalogo, carrito)
+mostrar_compra(catalogo, carrito)
