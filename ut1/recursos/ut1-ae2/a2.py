618489665532730da861a5ea7a0c10b004198076
Iván Chillón García <90761910+ichigar@users.noreply.github.com>
2022-01-20 19:55:10 +0000

    a2 terminado

--- ut1/recursos/ut1-ae2/a2.py
+++ ut1/recursos/ut1-ae2/a2.py
@@ -3,11 +3,15 @@ def count_phrases(text):
     Función a la que se le pasa un texto y devuelve el número de frases que contiene
     El caracter delimitador de frases es el punto '.'
     Tests: 
-    >>> count_phrases("Una frase. Dos frases. Tres frases. ")
+    
+    >>> texto = "Una frase. Dos frases. Tres frases."
+    >>> count_phrases(texto)
     3
     """
-    return len(text.split(". "))
+    phrases = text.split('. ')
     
+    return len(phrases)
+
 def phrases_in_text(text):
     """
     Función a la que se le pasa un texto y devuelve una lista con las frases que contiene
@@ -15,32 +19,36 @@ def phrases_in_text(text):
     Tests:
 
     >>> phrases_in_text("Una frase. Dos frases. Tres, cuatro, cinco; seis. Y siete.")
-    ['Una frase', 'Dos frases', 'Tres, cuatro, cinco; seis', 'siete']
+    ['Una frase', 'Dos frases', 'Tres, cuatro, cinco; seis', 'Y siete.']
     """
-    # Completar código
-    pass # Eliminar esta línea
-    
- 
+    return text.split('. ')
+
 def ordered_words(text):
     """
     Función a la que le pasamos un texto y devuelve una lista con las palabras que contiene
     La lista debe estar ordenada alfabéticamente
     Todas las palabras deben estar en minúscula
     
-     >>> ordered_words("Uno dos tres cuatro Cinco")
+    >>> ordered_words("Uno dos tres cuatro Cinco")
     ['cinco', 'cuatro', 'dos', 'tres', 'uno']
     >>> ordered_words("Uno, dos, tres; cuatro. Cinco")
     ['cinco', 'cuatro', 'dos', 'tres', 'uno']
     """
-    # Elimina mos signos de puntuación
-
-    # Pasamos a minúscula
+    # Eliminamos signos de puntuación
+    text = text.replace('.', '')
+    text = text.replace(';', '')
+    text = text.replace(',', '')
+    
+    # Pasar a minúscula texto
+    text = text.lower()
 
     # Obtenemos lista de palabras
+    lista = text.split()
 
-    # Ordenamos la lista y la devolvemos
-    pass # Eliminar esta línea
-    
+    # Devolvemos lista ordenada
+
+    return sorted(lista)
+
 def n_words_end_in(letter, text):
     """
     Función a la que se le pasa una letra y un texto y devuelve el número
@@ -52,8 +60,16 @@ def n_words_end_in(letter, text):
     >>> n_words_end_in("o", "Uno, dos, tres; cuatro. Cinco")
     3
     """
-    # Completar código
-    pass # Eliminar esta línea
+    # obtenemos lista de palabras en el texto
+    words = ordered_words(text)
+
+    # Calculamos palabras que terminan por la letra
+    count = 0
+    for word in words:
+        if word[-1] == letter:
+            count += 1
+    # devolvemos contador
+    return count
 
 def delete_word(text, word):
     """
@@ -61,13 +77,12 @@ def delete_word(text, word):
     sin que aparezca la palabra a eliminar.
     Tests:
 
-    >>> delete_word("dos", "Uno dos tres cuatro Cinco")
+    >>> delete_word("Uno dos tres cuatro Cinco", "dos")
     'Uno  tres cuatro Cinco'
-    >>> delete_word("dos", "Uno, dos, tres; cuatro. Cinco")
+    >>> delete_word("Uno, dos, tres; cuatro. Cinco", "dos")
     'Uno, , tres; cuatro. Cinco'
     """
-    # Completar código
-    pass # Eliminar esta línea
+    return text.replace(word, "")
     
 if __name__ == '__main__':
     import doctest
