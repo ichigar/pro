ff2ad9e9455d0b60be14af11134103e4f32f8473
Iván Chillón García <90761910+ichigar@users.noreply.github.com>
2022-02-03 20:15:01 +0000

    a2

--- ut1/recursos/ut1-ea2-b/a2.py
+++ ut1/recursos/ut1-ea2-b/a2.py
@@ -2,8 +2,30 @@ def cc2sc(identifier):
     """
     Converts text in camelCase format to snake_case format
     """
-    # Completar código de la función
-    pass # eliminar esta línea
+    # Pasar primer caracter a minúscula
+    first_char = identifier[0].lower()
+    result = first_char
+
+    aux = identifier[1:]
+    
+    # Recorremos por caracter
+
+    for caracter in aux:        
+        if caracter != caracter.lower():
+            result += '_' + caracter.lower()
+        else:
+            result += caracter
+
+    # Recorremos por índice
+    
+    # for i in range(1, len(identifier)):
+    #     caracter = identifier[i]
+    #     if caracter != caracter.lower():
+    #         result += '_' + caracter.lower()
+    #     else:
+    #         result += caracter
+       
+    return result
     
 print(cc2sc("nomVariable"))            # 'nom_variable'
 print(cc2sc("NomVariable"))            # 'nom_variable'
