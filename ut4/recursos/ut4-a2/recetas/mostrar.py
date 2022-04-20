653b1140e070bcd120957593609e2cc625552529
ichigar <ichigar@iesanaluisabenitez.org>
2022-04-20 20:53:28 +0100

    cambios en apariencia y nuevos botones

--- ut4/recursos/ut4-a2/recetas/mostrar.py
+++ ut4/recursos/ut4-a2/recetas/mostrar.py
@@ -8,34 +8,44 @@ class MostrarReceta:
         self._iniciar()
         
     def _iniciar(self):
-        self.op1_w = Tk()
-        self.op1_w.title("Mostrar Receta")
-        self.op1_w.geometry("400x300")
+        self.mostrar_w = Tk()
+        self.mostrar_w.title("Mostrar Receta")
+        self.mostrar_w.geometry("450x300")
         
-        self.lbl = Label(self.op1_w, text="Mostrar Receta\n", font=("Arial Bold", 12))
-        self.lbl.grid(column=0, row=0)
+        self.lbl = Label(self.mostrar_w, text="Mostrar Receta\n", font=("Arial Bold", 20))
+        self.lbl.grid(column=0, row=0, padx=5, pady=5)
 
-        self.lbl_n = Label(self.op1_w, text="Nombre de la receta:", font=("Arial", 12))    
-        self.lbl_n.grid(column=0, row=1)
         
-        self.txt_n = Entry(self.op1_w, width=25)
-        self.txt_n.grid(column=1, row=1)
+        self.lbl_n = Label(self.mostrar_w, text="Nombre de la receta:", font=("Arial", 12))    
+        self.lbl_n.grid(column=0, row=1, padx=10, pady=5, sticky = "w")
         
-        self.btn = Button(self.op1_w, text="Enviar", font=("Arial", 12), command = self._form1_enviar)
-        self.btn.grid(column=1, row=3)
+        self.txt_n = Entry(self.mostrar_w, width=25)
+        self.txt_n.grid(column=1, row=1, padx=5, pady=5)
+        self.txt_n.focus_set()           # Asignamos foco a este campo
         
-            
+        self.btn = Button(self.mostrar_w, text="Enviar", font=("Arial", 12), command = self._form1_enviar)
+        self.btn.grid(column=1, row=3, sticky='ew', padx=5, pady=5)
         
-        self.op1_w.mainloop()
+        # Al pulsar Enter ejecutar método
+        self.mostrar_w.bind('<Return>', self._form1_enviar)
         
-    def _form1_enviar(self):
+        self.mostrar_w.mainloop()
+        
+    def _form1_enviar(self, event = ""):        # bind pasa evento como parámetro
         nombre = self.txt_n.get()
         if nombre == "":
             messagebox.showerror("error", "No se ha introducido ningún valor")
+            # Ponemos la ventana de mostrar receta encima
+            self.mostrar_w.attributes('-topmost', True)
+            self.mostrar_w.update()
         else:
             receta = Receta()
             text_receta = receta.mostrar_receta(nombre)
             if not text_receta:
+
+                self.mostrar_w.destroy()
                 messagebox.showerror("error", "La receta no existe")
+                
             else:
+                self.mostrar_w.destroy()
                 messagebox.showinfo(nombre, text_receta)
