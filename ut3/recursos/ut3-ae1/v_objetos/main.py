8b4edf27fc6264dfab97fdc29b9f5a90e58afcb7
ichigar <ichigar@iesanaluisabenitez.org>
2022-04-20 17:54:00 +0100

    version con objetos del examen

---
+++ ut3/recursos/ut3-ae1/v_objetos/main.py
@@ -0,0 +1,22 @@
+from recetas import *
+
+receta = Receta()
+
+# receta.mostrar_receta("tortilla francesa")
+
+# receta.mostrar_receta_ingredientes(["aceite", "sal", "huevos"])
+
+# if receta.nueva_receta("arbejas", 15, ["arbejas", "zanahoria", "aceite", "sal"]):
+#     print("Receta añadida")
+# else:
+#     print("Receta ya existe")
+
+# if receta.añadir_ingrediente("tortilla francesa", "jamon"):
+#     print("Receta modificada")
+# else:
+#     print("Receta no existe o ya contienen el ingrediente")
+
+# if receta.eliminar_receta("tortilla francesa"):
+#     print("Receta eliminada")
+# else:
+#     print("Receta no existe")
